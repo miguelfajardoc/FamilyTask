5d0a18faed211e9cc0077bf7210b7b0fb9d4e0a4
Miguel Fajardo <miguel.fajardo@masivian.com>
2021-03-03 21:07:53 -0500

    [add] initial commit, models and migrations

---
+++ familyTaskAPI/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FamilytaskapiConfig(AppConfig):
+    name = 'familyTaskAPI'
---
+++ familyTaskAPI/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.contrib.auth.hashers import Argon2PasswordHasher
+
+class CommonInfo(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Family(CommonInfo):
+    
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class User(CommonInfo):
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True, null=False)
+    password = models.CharField(max_length=100)
+    family = models.ForeignKey(Family, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name + self.email
+
+class Rol(CommonInfo):
+
+    name = models.CharField(max_length=50)
+    abreviation = models.CharField(max_length=10)
+    user = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.abreviation
+
+class GeneralTask(CommonInfo):
+
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    programmed_date = models.DateTimeField()
+    estimatedTime = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    family = models.ForeignKey(Family, on_delete=models.CASCADE)
+
+class AsignedTask(CommonInfo):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    generalTask = models.ForeignKey(GeneralTask, on_delete=models.CASCADE)
+    asignationDate = models.DateTimeField()
+    realizationTime = models.FloatField()
+    state = models.CharField(max_length=100)
+    
+
+
+
