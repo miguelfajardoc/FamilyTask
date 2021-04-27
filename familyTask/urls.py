0cdaa9088bd9a48aa35e099cf27cc96599d2ae9b
Miguel Fajardo <miguel.fajardo@masivian.com>
2021-04-26 23:05:48 -0500

    [add] first family endpoint

--- familyTask/urls.py
+++ familyTask/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from familyTaskAPI import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('families/', views.FamilyView.as_view())
 ]
--- familyTaskAPI/views.py
+++ familyTaskAPI/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
+from .models import Family
+from django.core.serializers import serialize
 
-# Create your views here.
+class FamilyView(View):
+
+    def get(self, request):
+        families = Family.objects.all()
+        serialize_families = serialize('python', families)
+
+        data = {'families': serialize_families}
+        return JsonResponse(data)
+
